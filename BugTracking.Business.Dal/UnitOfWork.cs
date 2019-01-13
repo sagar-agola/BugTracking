c5bf0ebfba6d8e1d34dc03f966c9b96ab7fb3633
Sagar-N3o <n3o735@gmail.com>
2019-01-13 11:45:24 +0530

    started working on UnitOfWork

---
+++ BugTracking.Business.Dal/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BugTracking.Database.Domain;
+
+namespace BugTracking.Business.Dal
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly BugTrackingEntities context = new BugTrackingEntities();
+
+        public UnitOfWork()
+        {
+
+        }
+
+        private bool disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed && disposing)
+            {
+                context.Dispose();
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
